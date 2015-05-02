b40e7613e57dc9d1a0e3a53647adfa55a5efd5d2
James Curd <james.curd@razorfish.com>
2015-05-02 18:21:34 -0500

    Initial commit. Gulp is set up for styles and templates.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# aminimalanimal.github.io--source
-The source for aminimalanimal.github.io
+# aminimalanimal.github.io
+Personal portfolio project.
+
+## Technologies Used
+Precompilers: jade, sass, coffeescript
+Task-runner: Gulp
+
+## To run as dev
+- clone repo
+- navigate to repo
+- run `npm install`
+- run `bower install`
+- run `gulp`
+- your browser should automatically open the page
+
+## File Structure
+The `dist` folder isn't uploaded in this repo—this contains only the source files. To see the output, check out [github -- aminimalanimal.github.io](https://github.com/aminimalanimal/aminimalanimal.github.io--source).
+
+Structure is currently:
+
+	.
+	├── app
+		├── index.jade
+		├── scripts
+		├── styles
+		├── templates
+		└── bower_components
+	├── dist
+		├── index.html
+		├── scripts
+		├── styles
+		└── templates
+	├── gulpfile.js
+	├── package.json
+	├── node_modules
+	└── README.md
---
+++ gulpfile.js
@@ -0,0 +1,88 @@
+// REQUIRES
+// Gulp
+var gulp        = require('gulp');
+var gutil       = require('gutil');
+
+// Live Reloading
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+// Precompilers
+var jade        = require('gulp-jade');
+var sass        = require('gulp-sass');
+var coffee      = require('gulp-coffee');
+var sourcemaps  = require('gulp-sourcemaps');
+
+// Watcher
+var watch       = require('gulp-watch');
+
+
+// DIRECTORY STRUCTURE
+var DIR_SOURCE_INDEX     = './app/index.jade';
+var DIR_SOURCE_TEMPLATES = './app/templates/*.jade';
+var DIR_SOURCE_STYLES    = ['./app/styles/*.sass', './app/styles/*.scss'];
+var DIR_SOURCE_SCRIPTS   = ['./app/scripts/*.coffee', './app/scripts/*.litcoffee'];
+
+var DIR_BUILD_INDEX     = './dist';
+var DIR_BUILD_TEMPLATES  = './dist/templates';
+var DIR_BUILD_STYLES     = './dist/styles';
+var DIR_BUILD_SCRIPTS    = './dist/scripts';
+
+
+// TASKS
+
+// Compile Index
+gulp.task('index', function() {
+  return gulp.src(DIR_SOURCE_INDEX)
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(DIR_BUILD_INDEX));
+});
+
+// Compile Jade
+gulp.task('jade', function() {
+  return gulp.src(DIR_SOURCE_TEMPLATES)
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(DIR_BUILD_TEMPLATES));
+});
+
+// Compile Sass
+gulp.task('sass', function() {
+  return gulp.src(DIR_SOURCE_STYLES)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      indentedSyntax: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DIR_BUILD_STYLES));
+    // .pipe(reload({stream: true}));
+});
+
+// Compile Coffeescript
+gulp.task('coffee', function() {
+  return gulp.src(DIR_SOURCE_SCRIPTS)
+    .pipe(sourcemaps.init())
+    .pipe(coffee().on('error', gutil.log))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DIR_BUILD_SCRIPTS));
+});
+
+// Serve
+gulp.task('serve', ['index', 'jade', 'sass', 'coffee'], function() {
+  browserSync.init({
+    server: {
+      baseDir: DIR_BUILD_INDEX
+    }
+  });
+
+  gulp.watch(DIR_SOURCE_INDEX, ['index']).on('change', reload);
+  gulp.watch(DIR_SOURCE_TEMPLATES, ['jade']).on('change', reload);
+  gulp.watch(DIR_SOURCE_STYLES, ['sass']).on('change', reload);
+  gulp.watch(DIR_SOURCE_SCRIPTS, ['coffee']).on('change', reload);
+});
+
+// Default
+gulp.task('default', ['serve']);
