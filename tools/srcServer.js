93c597e691846cd25cc94df56c1a7b9d08eeb5bf
James Curd <james.curd@razorfish.com>
2016-03-20 15:53:55 -0500

    Dev nows opens in Google Chrome, rather than preferred browser.

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -41,6 +41,8 @@ browserSync({
     ]
   },
 
+  browser: 'google chrome',
+
   // no need to watch '*.js' here, webpack will take care of it for us,
   // including full page reloads if HMR won't work
   files: [
